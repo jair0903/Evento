aabc631811cb1e03eb0993171d532111c6d0127f
Jair Soto <jair@consucorp.com.mx>
2018-10-10 22:56:33 -0500

    Update evento-frontEnd/src/app/material.modules.ts

--- evento-frontEnd/src/app/material.modules.ts
+++ evento-frontEnd/src/app/material.modules.ts
@@ -31,7 +31,6 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatTableModule} from '@angular/material/table';
 import {MatSortModule} from '@angular/material/sort';
 import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatFormFieldModule} from '@angular/material/form-field';
 
 
 @NgModule({
@@ -67,8 +66,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatSnackBarModule,
     MatTableModule,
     MatSortModule,
-	MatPaginatorModule,
-    MatFormFieldModule],
+	MatPaginatorModule],
 
   exports: [BrowserModule,
     BrowserAnimationsModule,
@@ -102,7 +100,6 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatSnackBarModule,
     MatTableModule,
     MatSortModule,
-	MatPaginatorModule,
-    MatFormFieldModule],
+	MatPaginatorModule],
 })
 export class MaterialModule { }
