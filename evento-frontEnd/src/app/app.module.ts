3e733c196aa1e29835553a5356fc2ad505d9faca
Jair Soto <jair@consucorp.com.mx>
2018-10-10 21:55:10 -0500

    Configuración de Modulos y dependencias

--- evento-frontEnd/src/app/app.module.ts
+++ evento-frontEnd/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+// Angular Animations
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+// Angular Material Modules
+import {MaterialModule} from './material.modules';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
